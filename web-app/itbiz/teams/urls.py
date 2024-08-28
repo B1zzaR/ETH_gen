dbac6cc9abc08e370d89644f8ce2684882f3c9a7
De. B1zaR <154279394+B1zzaR@users.noreply.github.com>
2024-08-28 23:55:39 +0300

    Delete web-app directory

--- web-app/itbiz/teams/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.teams_home, name="teams_home"),
-    path('create', views.create, name="create"),
-    path('<int:pk>', views.TeamDetailView.as_view(), name='teams-detail')
-]
--- web-app/itbiz/Текстовый документ.txt
+++
@@ -1,4 +0,0 @@
-@echo off
-cd itbiz
-python manage.py runserver
-pause
